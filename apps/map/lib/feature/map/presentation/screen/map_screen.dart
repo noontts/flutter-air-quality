bb95234358c4075a3c2d24dd94da23cc48e9f40d
thitisan.k <noonthitisan@gmail.com>
2024-05-14 19:32:22 +0700

    chore: .gitignore

--- apps/map/lib/feature/map/presentation/screen/map_screen.dart
+++ apps/map/lib/feature/map/presentation/screen/map_screen.dart
@@ -1,3 +1,4 @@
+import 'package:core_libs/dependency_injection/get_it.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
