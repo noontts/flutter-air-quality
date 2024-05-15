f48e9373aee4a107a7e0b017afe09c93050c3e24
Thitisan K <noonthitisan@gmail.com>
2024-05-15 14:31:06 +0700

    test: http service.

---
+++ apps/map/test/mocks/mock_http.dart
@@ -0,0 +1,36 @@
+import 'package:core_libs/networks/http/http_service.dart';
+
+class MockHttpService extends HttpService{
+  dynamic data;
+  MockHttpService(super.url);
+
+  @override
+  Future delete(String path, data) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future get(String path) async{
+    return await data;
+  }
+
+  @override
+  Future patch(String path, data) {
+    // TODO: implement patch
+    throw UnimplementedError();
+  }
+
+  @override
+  Future post(String path, data) {
+    // TODO: implement post
+    throw UnimplementedError();
+  }
+
+  @override
+  Future put(String path, data) {
+    // TODO: implement put
+    throw UnimplementedError();
+  }
+  
+}
