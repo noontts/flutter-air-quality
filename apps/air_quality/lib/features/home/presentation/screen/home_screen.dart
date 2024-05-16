f49df4a28ef0e007e265c0e008fe670f574c7089
Thitisan K <noonthitisan@gmail.com>
2024-05-16 16:52:31 +0700

    test: call api.

--- apps/air_quality/lib/features/home/presentation/screen/home_screen.dart
+++ apps/air_quality/lib/features/home/presentation/screen/home_screen.dart
@@ -1,6 +1,10 @@
+import 'package:air_quality/features/home/domain/entities/aqi.dart';
+import 'package:air_quality/features/home/domain/ports/aqi/services.dart';
 import 'package:air_quality/features/home/presentation/widget/city_air_quality_zone.dart';
 import 'package:air_quality/features/home/presentation/widget/current_observation.dart';
+import 'package:core_libs/dependency_injection/get_it.dart';
 import 'package:flutter/material.dart';
+import 'package:latlong2/latlong.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -10,6 +14,24 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  final mockCurrentLatLng = const LatLng(18.80823885274427, 98.9541342695303);
+  IAQIService service = getIt.get<IAQIService>();
+  late AqiToDisplay aqiToDisplay;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentAqiDetail(mockCurrentLatLng);
+  }
+
+  void getCurrentAqiDetail(LatLng latLng) async{
+    final response = await service.getAqiDetailByLatLng(latLng);
+
+    setState(() {
+      aqiToDisplay = response;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
