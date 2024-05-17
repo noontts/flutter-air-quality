c822e26eb4f996771ff02f01fb0a1b425c68d630
Thitisan K <noonthitisan@gmail.com>
2024-05-17 18:20:01 +0700

    feat: add loading screen.

---
+++ apps/air_quality/lib/features/home/presentation/widget/loading.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class LoadingFullScreen extends StatelessWidget {
+  const LoadingFullScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Lottie.asset("../core/assets/lotties/loading.json"),
+    );
+  }
+}
